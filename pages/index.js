09f614b7a019e814bf60922fe50017e4039e6aa4
wuyanxubarhoumi <67730700+wuyanxubarhoumi@users.noreply.github.com>
2022-01-03 23:39:16 -0600

    Add prop called highlighted to Button

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function HomePage({}) {
       </HeadingContainer>
       <Pie>
         {categoryData.length === 0 ? (
-          <p>It seems like there is no expense to display 🤪</p>
+          <Message>Oops! No expense to display for the {duration} 🤪</Message>
         ) : (
           <VictoryPie
             data={categoryData}
@@ -45,9 +45,24 @@ export default function HomePage({}) {
         )}
       </Pie>
       <ButtonGroup>
-        <Button onClick={() => setDuration('day')}>Day</Button>
-        <Button onClick={() => setDuration('week')}>Week</Button>
-        <Button onClick={() => setDuration('month')}>Month</Button>
+        <Button
+          highlighted={duration === 'day'}
+          onClick={() => setDuration('day')}
+        >
+          Day
+        </Button>
+        <Button
+          highlighted={duration === 'week'}
+          onClick={() => setDuration('week')}
+        >
+          Week
+        </Button>
+        <Button
+          highlighted={duration === 'month'}
+          onClick={() => setDuration('month')}
+        >
+          Month
+        </Button>
       </ButtonGroup>
     </Wrapper>
   )
@@ -59,9 +74,10 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 40px;
-  gap: 60px;
+  gap: 25px;
+  padding: 60px 0;
   position: relative;
+  overflow: none;
 
   @media ${QUERIES.tabletAndBigger} {
     padding: 100px;
@@ -72,6 +88,7 @@ const HeadingContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  padding: 0 40px;
 `
 
 const Heading = styled.h1`
@@ -83,18 +100,23 @@ const TimeVar = styled.span`
   font-family: var(--font-cursive);
   font-size: ${20 / 16}rem;
   text-transform: uppercase;
-  color: #a63c06;
+  color: #c3500c;
+`
+
+const Message = styled.p`
+  padding: 60px 40px;
 `
 
 const Pie = styled.div`
   font-family: var(--font-sans-serif);
+  padding-right: 10px;
 `
 
 const ButtonGroup = styled.div`
   display: flex;
   gap: 30px;
   position: absolute;
-  top: 550px;
+  top: 600px;
   @media ${QUERIES.tabletAndBigger} {
     top: 650px;
     gap: 50px;
@@ -102,13 +124,14 @@ const ButtonGroup = styled.div`
 `
 
 const Button = styled.button`
-  background-color: #c2fbd7;
+  background-color: ${({ highlighted }) =>
+    highlighted ? '#ffd60a' : '#c2fbd7'};
   border-radius: 100px;
-  box-shadow: rgba(44, 187, 99, 0.2) 0 -25px 18px -14px inset,
+  /* box-shadow: rgba(44, 187, 99, 0.2) 0 -25px 18px -14px inset,
     rgba(44, 187, 99, 0.15) 0 1px 2px, rgba(44, 187, 99, 0.15) 0 2px 4px,
     rgba(44, 187, 99, 0.15) 0 4px 8px, rgba(44, 187, 99, 0.15) 0 8px 16px,
-    rgba(44, 187, 99, 0.15) 0 16px 32px;
-  color: green;
+    rgba(44, 187, 99, 0.15) 0 16px 32px; */
+  color: ${({ highlighted }) => (highlighted ? '#a63c06' : 'green')};
   cursor: pointer;
   display: inline-block;
   font-family: CerebriSans-Regular, -apple-system, system-ui, Roboto, sans-serif;
@@ -130,14 +153,5 @@ const Button = styled.button`
       rgba(255, 214, 10, 0.25) 0 4px 8px, rgba(255, 214, 10, 0.25) 0 8px 16px,
       rgba(255, 214, 10, 0.25) 0 16px 32px;
     transform: scale(1.05) rotate(-1deg);
-  }
-
-  &:focus {
-    background-color: #ffd60a;
-    color: #a63c06;
-    box-shadow: rgba(255, 214, 10, 0.35) 0 -25px 18px -14px inset,
-      rgba(255, 214, 10, 0.25) 0 1px 2px, rgba(255, 214, 10, 0.25) 0 2px 4px,
-      rgba(255, 214, 10, 0.25) 0 4px 8px, rgba(255, 214, 10, 0.25) 0 8px 16px,
-      rgba(255, 214, 10, 0.25) 0 16px 32px;
   }
 `
