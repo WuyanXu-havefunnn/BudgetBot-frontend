c24ede1217e47cea6e1b1180129ad8598ee316b2
wuyanxubarhoumi <67730700+wuyanxubarhoumi@users.noreply.github.com>
2022-01-07 13:54:38 -0600

    Remove console.log

--- airtable.js
+++ airtable.js
@@ -1,7 +1,5 @@
 import Airtable from 'airtable'
 
-console.log(process.env.API_KEY)
-
 export const AirtableBase = new Airtable({ apiKey: process.env.API_KEY }).base(
   process.env.BASE_ID,
 )
--- pages/api/expenses/[duration].js
+++ pages/api/expenses/[duration].js
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
   res.status(200).json(collectedCategoryData)
 }
 
-const today = new Date()
 const yesterday = new Date()
 yesterday.setDate(yesterday.getDate() - 1)
 yesterday.setHours(0, 0, 0, 0)
